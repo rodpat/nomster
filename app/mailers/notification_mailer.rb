535b8caed376afd03fd70a2d1200c41fbf698bdf
Rod <rodpat@gmail.com>
2017-08-15 16:22:15 +0000

    Previewing the Automated Email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,8 +2,8 @@ class NotificationMailer < ApplicationMailer
   default from: "no-reply@nomsterapp.com"
 
   def comment_added
-    mail(to: "rodpat@gmail.com", 
-         subject: "A coment has been added to your place")
+    mail(to: "testrodprojects@gmail.com", 
+        subject: "A comment has been added to your place")
   end
 
 end
