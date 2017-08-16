d65e177df3c5463a7006145eef45ddb6fba9591c
Rod <rodpat@gmail.com>
2017-08-16 05:19:33 +0000

    Modifiying production.rb file to send emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,14 +1,6 @@
 Rails.application.configure do
 
-  ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'heroku.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
+ 
   
   config.action_mailer.delivery_method = :test
   #config.action_mailer.delivery_method = :smtp
