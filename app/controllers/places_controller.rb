5dc4bbaf2efc702d831a5115891d0e72581d8da4
Rod <rodpat@gmail.com>
2017-08-19 02:38:32 +0000

    Adding the Places a user created to their profile page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,7 @@ class PlacesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
   def index
-    @places = Place.all.order("created_at DESC").paginate(page: params[:page], :per_page =>10)
+    @places = Place.all.order("created_at DESC").paginate(page: params[:page], :per_page =>5)
     @photo = Photo.new
   end
 
